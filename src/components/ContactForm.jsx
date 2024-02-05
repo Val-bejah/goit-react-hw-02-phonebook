a6ea8f789006c0cc3946574f368a5650baeb0b8f
Val-bejah <116029510+Val-bejah@users.noreply.github.com>
2024-02-05 07:18:30 -0500

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -8,13 +8,13 @@ function ContactForm({ contacts, setContacts }) {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  // const handleNameChange = e => {
-  //   setName(e.target.value);
-  // };
+  const handleNameChange = e => {
+    setName(e.target.value);
+  };
 
-  // const handleNumberChange = e => {
-  //   setNumber(e.target.value);
-  // };
+  const handleNumberChange = e => {
+    setNumber(e.target.value);
+  };
 
   const nameExists = newName => {
     return contacts.some(
@@ -54,6 +54,9 @@ function ContactForm({ contacts, setContacts }) {
         <input
           type="text"
           name="name"
+          id="name"
+          value={name}
+          onChange={handleNameChange}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
@@ -64,8 +67,10 @@ function ContactForm({ contacts, setContacts }) {
         Number:
         <input
           type="tel"
+          name="number"
+          id="number"
           value={number}
-          onChange={e => setNumber(e.target.value)}
+          onChange={handleNumberChange}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
